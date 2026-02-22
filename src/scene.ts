2c6a39eee82ae4b15810678c3f25557bf500f5dc
dishimwe1000-codder <dishimwe1000@gmail.com>
2026-02-22 15:23:14 +0200

    Update src/scene.ts with new scene management implementation

--- src/scene.ts
+++ src/scene.ts
@@ -1,37 +1,54 @@
 import * as THREE from 'three';
 
 export class Scene {
-  private scene: THREE.Scene;
+    private objects: Map<string, THREE.Object3D>;
+    private parentMap: Map<string, string | null>;
 
-  constructor() {
-    this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0x1a1a1a);
-  }
+    constructor() {
+        this.objects = new Map();
+        this.parentMap = new Map();
+    }
 
-  public add(object: THREE.Object3D): void {
-    this.scene.add(object);
-  }
-
-  public remove(object: THREE.Object3D): void {
-    this.scene.remove(object);
-  }
-
-  public getThreeScene(): THREE.Scene {
-    return this.scene;
-  }
-
-  public dispose(): void {
-    this.scene.traverse((child) => {
-      if (child instanceof THREE.Mesh) {
-        child.geometry.dispose();
-        if (Array.isArray(child.material)) {
-          child.material.forEach((mat) => mat.dispose());
-        } else {
-          child.material.dispose();
+    add(object: THREE.Object3D, parentId: string | null = null): void {
+        this.objects.set(object.id, object);
+        this.parentMap.set(object.id, parentId);
+        if (parentId) {
+            const parent = this.objects.get(parentId);
+            if (parent) {
+                parent.add(object);
+            }
         }
-      }
-    });
-  }
-}
+    }
 
-export default Scene;
+    remove(objectId: string): void {
+        const object = this.objects.get(objectId);
+        if (object) {
+            const parentId = this.parentMap.get(objectId);
+            if (parentId) {
+                const parent = this.objects.get(parentId);
+                if (parent) {
+                    parent.remove(object);
+                }
+            }
+            this.objects.delete(objectId);
+            this.parentMap.delete(objectId);
+        }
+    }
+
+    get(objectId: string): THREE.Object3D | undefined {
+        return this.objects.get(objectId);
+    }
+
+    // Function to get all child objects of a parent
+    getChildren(parentId: string): THREE.Object3D[] {
+        const children: THREE.Object3D[] = [];
+        this.objects.forEach((object, id) => {
+            if (this.parentMap.get(id) === parentId) {
+                children.push(object);
+            }
+        });
+        return children;
+    }
+
+    // Additional utility functions can be added below
+}
